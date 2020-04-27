603858d279f8405082c240b03c34972ceffa9a5c
Shivam Panjeta <shivam.panjeta@gmail.com>
2020-04-27 18:15:18 +0530

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -1,5 +1,20 @@
+from flask import Flask
+from flask import request, jsonify, abort, make_response
 from gensim.summarization.summarizer import summarize
 
-def lambda_handler(event, context):
-    text = text_content
-    return summarize(text)
+app = Flask(__name__)
+
+@app.route('/summarize', methods=['POST'])
+def lambda_handler():
+    data = request.data
+    summary = summarize(data)
+    if not data:
+        abort(make_response(jsonify(message="Request must have raw text"), 400))
+
+    return jsonify({
+        'summary': summary
+    })
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
