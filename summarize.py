b9ab5c3c7ffc6884e50419fb5e6389ba3d0edd85
Shivam Panjeta <shivam.panjeta@gmail.com>
2020-04-27 20:10:40 +0530

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -19,5 +19,34 @@ def lambda_handler():
     })
 
 
+def start_server():
+    logging.info('Starting the inference server with {} workers.'.format(model_server_workers))
+
+    # link the log streams to stdout/err so they will be logged to the container logs
+    subprocess.check_call(['ln', '-sf', '/dev/stdout', '/var/log/nginx/access.log'])
+    subprocess.check_call(['ln', '-sf', '/dev/stderr', '/var/log/nginx/error.log'])
+
+    nginx = subprocess.Popen(['nginx', '-c', '/opt/program/nginx.conf'])
+    gunicorn = subprocess.Popen(['gunicorn',
+                                 '--timeout', str(model_server_timeout),
+                                 '-k', 'gevent',
+                                 '-b', 'unix:/tmp/gunicorn.sock',
+                                 '-w', str(model_server_workers),
+                                 'wsgi:app'])
+
+    signal.signal(signal.SIGTERM, lambda a, b: sigterm_handler(nginx.pid, gunicorn.pid))
+
+    # If either subprocess exits, so do we.
+    process_ids = {nginx.pid, gunicorn.pid}
+    while True:
+        pid, _ = os.wait()
+        if pid in process_ids:
+            break
+
+    sigterm_handler(nginx.pid, gunicorn.pid)
+    logging.info('Inference server exiting')
+
+
+# The main routine just invokes the start function.
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    start_server()
